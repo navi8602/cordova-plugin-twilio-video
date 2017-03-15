c1759082cb723b096f7c54cbdd4497c664f0451e
ghaiat <ghaiat@ipelia.fr>
2017-03-15 12:39:56 +0000

    WIP

--- src/android/TwilioVideoActivity.java
+++ src/android/TwilioVideoActivity.java
@@ -38,7 +38,7 @@ import com.twilio.video.VideoView;
 
 import android.content.Intent;
 
-//import org.apache.cordova.plugin.TwilioVideo.R;
+//import org.apache.cordova.plugin.R;
 import android.R;
 
 import java.util.Map;
